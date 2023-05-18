206a9b94e393738dd3a7644c9dbc823f73016cd2
Lila Rest <mail@lila.rest>
2023-05-18 13:17:07 +0200

    feat: cleanup Next project to make it blank

---
+++ next.config.mjs
@@ -0,0 +1,12 @@
+// This ensure env vars are validated at build-time
+// See: https://env.t3.gg/docs/nextjs
+// import "./src/env.mjs";
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    // typedRoutes: true, // Enable internal link type-checking (see: https://nextjs.org/docs/pages/building-your-application/configuring/typescript#statically-typed-links)
+  },
+};
+export default nextConfig;
